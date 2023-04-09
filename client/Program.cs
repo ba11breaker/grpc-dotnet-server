1f8c83c601f01e5c87cf47c114cdee0bd2896199
Alex Zhao <alex.zhao@go1.com>
2023-04-09 19:56:31 +1000

    feat: add GreetClient Boilerplate Code

--- client/Program.cs
+++ client/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Core;
+using Dummy;
+
+const string target = "127.0.0.1:50051";
+
+// Create a channel
+Channel channel = new Channel(target, ChannelCredentials.Insecure);
+
+channel.ConnectAsync().ContinueWith((task) =>
+{
+    if (task.Status == TaskStatus.RanToCompletion)
+    {
+        Console.WriteLine("The client connected successfully");
+    }
+});
+
+// Create a client
+var client = new DummyService.DummyServiceClient(channel);
+
+channel.ShutdownAsync().Wait();
+Console.ReadKey();
